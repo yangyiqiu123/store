8e9f4fb266d7cf2a781e8c8aa61c6d881eb8a052
cL2G7EneYG <yangyiqiulearn@gmail.com>
2023-07-03 16:02:05 +0800

    b

--- Itsa_basis_40/19.py
+++ Itsa_basis_40/19.py
@@ -23,19 +23,43 @@
 # print(ans)
 
 
-n = int(input())
-times = list(map(int, input().split()))
+# n = int(input())
+# times = list(map(int, input().split()))
 
-events = []
-for i in range(0, len(times), 2):
-    events.append((times[i], 1)) 
-    events.append((times[i + 1], -1)) 
-events.sort()
+# events = []
+# for i in range(0, len(times), 2):
+#     events.append((times[i], 1)) 
+#     events.append((times[i + 1], -1)) 
+# events.sort()
 
-current_tasks = 0
-max_tasks = 0
-for event in events:
-    current_tasks += event[1]
-    max_tasks = max(max_tasks, current_tasks)
+# current_tasks = 0
+# max_tasks = 0
+# for event in events:
+#     current_tasks += event[1]
+#     max_tasks = max(max_tasks, current_tasks)
 
-print(max_tasks)
+# print(max_tasks)
+
+
+while True:
+    try:
+        order=int(input())
+        time=list(map(int,input().split()))
+        cars=order
+        
+        for i in range(0,len(time),2):
+            for j in range(1,len(time),2): 
+                if time[i]-time[j]>=0 :
+                    
+                    time[j]=time[i+1]
+                    
+                    cars-=1
+                    
+                    break
+                    
+        if cars == 0:
+            print(1)
+        else:
+            print(cars)
+    except EOFError:
+        break
